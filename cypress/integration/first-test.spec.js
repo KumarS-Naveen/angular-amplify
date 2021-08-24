f229936c1401097f827e40f093fd7fe572e386c5
NaveenKumarS <naveenkumar.s@siemens.com>
2021-08-24 10:11:32 +0530

    init

---
+++ cypress/integration/first-test.spec.js
@@ -0,0 +1,16 @@
+describe("sign in", () => {
+    it("should visit login page", () => {
+      cy.visit("http://localhost:4200");
+      cy.get(selectors.usernameInput).type("test");
+      cy.get(selectors.signInPasswordInput).type("@Test123");
+      cy.get(selectors.signInSignInButton).contains('Sign In').click();
+    });
+  });
+
+  export const selectors = {
+    // Auth component classes
+    usernameInput: '[data-test="sign-in-username-input"]',
+    signInPasswordInput: '[data-test="sign-in-password-input"]',
+    signInSignInButton: '[data-test="sign-in-sign-in-button"]',
+    signOutButton: '[data-test="sign-out-button"]'
+  }
