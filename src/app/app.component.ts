752613e8ef113a95b97f9d4f0ed1ac926cadbffc
NaveenKumarS <naveenkumar.s@siemens.com>
2021-08-28 19:52:46 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent implements OnInit {
   constructor(private fb: FormBuilder,private http: HttpClient) { }
   ngOnInit(): void {
     const url = "http://nodebff-env-1.eba-pn2xek6t.ap-south-1.elasticbeanstalk.com/user";
-    this.http.get("/api").subscribe(data => {
+    this.http.get("/api/user").subscribe(data => {
       this.appname = data.toString();
     });
     this.createForm = this.fb.group({
