6f6770d58f522cbe714e1c15ba72191eda5f6d3c
NaveenKumarS <naveenkumar.s@siemens.com>
2021-08-28 11:31:26 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ export class AppComponent implements OnInit {
   /* declare restaurants variable */
   constructor(private fb: FormBuilder,private http: HttpClient) { }
   ngOnInit(): void {
-    const url = `http://nodebff-env-1.eba-pn2xek6t.ap-south-1.elasticbeanstalk.com/user`;
+    const url = "http://nodebff-env-1.eba-pn2xek6t.ap-south-1.elasticbeanstalk.com/user";
     this.http.get(url).subscribe(data => {
       this.appname = data.toString();
     });
