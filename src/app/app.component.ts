9fe8c6c8a7d3bc3a793270b546324a4cb546a66c
NaveenKumarS <naveenkumar.s@siemens.com>
2021-08-28 19:46:21 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,14 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  appname:string ="";
+  appname:string ="hisdsd";
   title = 'amplify-app';
   public createForm!: FormGroup;
   /* declare restaurants variable */
   constructor(private fb: FormBuilder,private http: HttpClient) { }
   ngOnInit(): void {
     const url = "http://nodebff-env-1.eba-pn2xek6t.ap-south-1.elasticbeanstalk.com/user";
-    this.http.get(url).subscribe(data => {
+    this.http.get("/api").subscribe(data => {
       this.appname = data.toString();
     });
     this.createForm = this.fb.group({
