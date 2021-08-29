6eba20dbfc70c939a05b84ba1668785af0e07b02
NaveenKumarS <naveenkumar.s@siemens.com>
2021-08-29 16:52:26 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent implements OnInit {
   constructor(private fb: FormBuilder,private http: HttpClient) { }
   ngOnInit(): void {
     // const url = "http://nodebff-env-1.eba-pn2xek6t.ap-south-1.elasticbeanstalk.com/user";
-    this.http.get("https://api.instantwebtools.net/v1/airlines").subscribe(data => {
+    this.http.get("/api").subscribe(data => {
       console.log(data);
      // this.appname = data.toString(); 
     });
